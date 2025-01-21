3fa007ea39b1bfa7598a32755cc8901b772fd19d
Aditya Shrivastava <aditya@treety.co>
2025-01-21 11:06:17 -0500

    refined calculator function

--- calculator-frontend/src/Calculator.js
+++ calculator-frontend/src/Calculator.js
@@ -4,21 +4,22 @@ import "./Calculator.css"; // Optional for styling
 
 const Calculator = () => {
   const [expression, setExpression] = useState("");
-  const [result, setResult] = useState("");
 
   const handleButtonClick = (value) => {
     if (value === "=") {
       // Send expression to backend
+      setExpression("");
       axios
         .post("http://localhost:5001/calculate", { expression })
-        .then((response) => setResult(response.data.result))
+        .then((response) => {
+          setExpression(response.data.result);
+        })
         .catch((error) => {
             console.log(error);
             alert("Error calculating expression");
         });
     } else if (value === "C") {
       setExpression("");
-      setResult("");
     } else {
       setExpression(expression + value);
     }
@@ -28,7 +29,6 @@ const Calculator = () => {
     <div className="calculator">
       <div className="display">
         <div className="expression">{expression}</div>
-        <div className="result">{result}</div>
       </div>
       <div className="buttons">
         {["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "C", "+", "="].map((button) => (
